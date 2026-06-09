e77653a06bf0d491523b1b0e7a33017e06c92898
Satriyo62 <satriyoyunan@gmail.com>
2026-06-09 14:23:01 +0700

    Perubahan inisialisasi database

--- app/src/main/java/id/ac/pnm/edushare/EduShareApp.kt
+++ app/src/main/java/id/ac/pnm/edushare/EduShareApp.kt
@@ -1,23 +1,15 @@
 package id.ac.pnm.edushare
 
 import android.app.Application
-import androidx.room.Room
 import id.ac.pnm.edushare.data.local.AppDatabase
 
 class EduShareApp : Application() {
     companion object {
-
         lateinit var db: AppDatabase
-
     }
 
     override fun onCreate() {
         super.onCreate()
-
-        db = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase::class.java,
-            "edushare_db"
-        ).build()
+        db = AppDatabase.getDatabase(applicationContext)
     }
 }
